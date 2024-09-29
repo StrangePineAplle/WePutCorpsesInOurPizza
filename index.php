0a72cd36523a4f7de55114160c5b56b89c269fe2
StrangePineAplle <shadow77777777@mail.ru>
2024-09-29 23:44:30 +0300

    SUPER FIX ISO SO COOL YES STABLE VERSION ALSO

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ $dishes = sqlrequest("SELECT * FROM dish");
         </div>
 
        <!-- Попап для корзины -->
-<<<<<<< HEAD
+
 <div id="cartPopup" class="popup cart-popup" style="display:none;">
     <div class="popup-content auth-popup-content">
         <span class="close" onclick="closeCartPopup('cartPopup')">&times;</span>
@@ -60,18 +60,6 @@ $dishes = sqlrequest("SELECT * FROM dish");
         <div id="cartItems">
             <!-- Здесь будут отображаться товары в корзине -->
             <p>Ваша корзина пуста.</p> <!-- Сообщение по умолчанию -->
-=======
-        <div id="cartPopup" class="popup cart-popup" style="display:none;">
-            <div class="popup-content auth-popup-content">
-                <span class="close" onclick="closeCartPopup()">&times;</span>
-                <h2>Корзина</h2>
-                <div id="cartItems">
-                    <!-- Здесь будут отображаться товары в корзине -->
-                    <p>Ваша корзина пуста.</p> <!-- Сообщение по умолчанию -->
-                </div>
-                <button id="checkoutButton" class="auth-submit-button">Оформить заказ</button>
-            </div>
->>>>>>> a837cf1667b70e3556fc20d8e44ddff623126c61
         </div>
         <button id="checkoutButton" class="auth-submit-button">Оформить заказ</button>
     </div>
